61bc0591fab549ab4d35f85c4544d97efe0668f4
Bruno Bartolomasi <brunobrt@hotmail.com>
2020-06-18 18:33:01 -0300

    Criação de um app que joga dados

---
+++ android/app/src/main/java/brunobartolomasi/dice/MainActivity.java
@@ -0,0 +1,6 @@
+package brunobartolomasi.dice;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
