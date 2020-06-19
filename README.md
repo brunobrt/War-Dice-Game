afef28b0b224445be451cf66fdcc20e054a6b700
Bruno Bartolomasi <brunobrt@hotmail.com>
2020-06-18 22:04:29 -0300

    6 dados, 3 ataque e 3 defesa

--- README.md
+++ README.md
@@ -1,12 +1,9 @@
-# Dice
+# War Dice Game
 
-![Screenshot_1592523083](https://user-images.githubusercontent.com/30990524/85081580-b5bb7100-b1a2-11ea-8471-dbbd5274c15f.png)
+Um app inspirado nos dados do jogo War.
 
-Esse app foi feito como parte dos meus estudos de Flutter.
+Dados vermelhos são do ataque e os amarelos da defesa.
 
-Ao clicar no dado, ele joga o dado novamente e mostra o novo número .
+Cada jogador joga 3 dados por vez, então, ao tocar em um dado de ataque os 3 são atualizados com valores aleatórios. 
+O mesmo acontece para o jogador da defesa.
 
-Os dados são independentes, apenas o dado clicado é atualizado.
-
-
->Esse projeto foi desenvolvido para o curso App Brewery's Complete Flutter Development Bootcamp, veja o curso inteiro em [www.appbrewery.co](https://www.appbrewery.co/)
--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,15 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.blueGrey.shade200,
         appBar: AppBar(
-          title: Center(child: Text('Dice Roller')),
-          backgroundColor: Colors.blue.shade500,
+          title: Center(child: Text('War Dice Game',
+            style: TextStyle(
+              fontFamily: 'Staatliches',
+              fontSize: 36.0,
+            color: Colors.black
+            ),
+          ),
+          ),
+          backgroundColor: Colors.orange.shade200,
         ),
         body: DicePage(),
       ),
@@ -23,37 +30,117 @@ class DicePage extends StatefulWidget {
 
 class _DicePageState extends State<DicePage> {
 
-  int leftDiceNumber = 1;
-  int rightDiceNumber = 1;
+  int attackerLeftDiceNumber = 1;
+  int attackerMiddleDiceNumber = 1;
+  int attackerRightDiceNumber = 1;
+  int defenderLeftDiceNumber = 1;
+  int defenderMiddleDiceNumber = 1;
+  int defenderRightDiceNumber = 1;
+
+  void changeDiceNumberAttack() {
+    setState(() {
+      attackerLeftDiceNumber = Random().nextInt(6) + 1;
+      attackerMiddleDiceNumber = Random().nextInt(6) + 1;
+      attackerRightDiceNumber = Random().nextInt(6) + 1;
+    },);
+  }
+  void changeDiceNumberDefend() {
+    setState(() {
+      defenderLeftDiceNumber = Random().nextInt(6) + 1;
+      defenderMiddleDiceNumber = Random().nextInt(6) + 1;
+      defenderRightDiceNumber = Random().nextInt(6) + 1;
+    },);
+  }
 
   @override
   Widget build(BuildContext context) {
 
-    return Center(
-      child: Row(
-        children: <Widget>[
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  leftDiceNumber = Random().nextInt(6) + 1;
-                },);
-              },
-              child: Image.asset('images/dice$leftDiceNumber.png'),
-            ),
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        ListTile(
+          title: Center(child: Text('Attacker',
+          style: TextStyle(
+            fontWeight: FontWeight.bold
+          ),),
           ),
-          Expanded(
-            child: FlatButton(
-              onPressed: () {
-                setState(() {
-                  rightDiceNumber = Random().nextInt(6) + 1;
-                },);
-              },
-              child: Image.asset('images/dice$rightDiceNumber.png'),
-            ),
+        ),
+        Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceNumberAttack();
+                  },
+                  child: Image.asset('images/diceAttack$attackerLeftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceNumberAttack();
+                  },
+                  child: Image.asset('images/diceAttack$attackerMiddleDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceNumberAttack();
+                  },
+                  child: Image.asset('images/diceAttack$attackerRightDiceNumber.png'),
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
+        ),
+        SizedBox(
+          height: 80.0,
+          width: 300.0,
+          child: Divider(
+            color: Colors.blueGrey.shade500,
+          ),
+        ),
+
+        Center(
+          child: Row(
+            children: <Widget>[
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceNumberDefend();
+                  },
+                  child: Image.asset('images/diceDefend$defenderLeftDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceNumberDefend();
+                  },
+                  child: Image.asset('images/diceDefend$defenderMiddleDiceNumber.png'),
+                ),
+              ),
+              Expanded(
+                child: FlatButton(
+                  onPressed: () {
+                    changeDiceNumberDefend();
+                  },
+                  child: Image.asset('images/diceDefend$defenderRightDiceNumber.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
+        ListTile(
+          title: Center(child: Text('Defender',
+          style: TextStyle(
+            fontWeight: FontWeight.bold
+          ),),
+          ),
+        ),
+      ],
     );
   }
 }
